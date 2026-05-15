036abd0ee334af0a7b71ef4e3ef1fb30e9cac09d
MKKlausen <mathias.karstoft.klausen@gmail.com>
2026-05-15 22:20:28 +0200

    Renamed PlayShuffleLoadCmd to AfspilCmd

--- src/main/java/com/jagrosh/jmusicbot/commands/CommandFactory.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/CommandFactory.java
@@ -40,7 +40,7 @@ public class CommandFactory {
                     // new LyricsCmd(bot),
                     new NowPlayingCmd(bot),
                     new PlayCmd(bot),
-                    new PlayShuffleLoadCmd(bot),
+                    new AfspilCmd(bot),
                     new PlaylistsCmd(bot),
                     new QueueCmd(bot),
                     new RemoveCmd(bot),
